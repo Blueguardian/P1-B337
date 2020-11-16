4de651497e56dc69bc5cfe1f4f2f5a495c1978fa
Daniel Doroshenko <daniel.d@outlook.dk>
2020-11-16 12:44:20 +0100

    Important change in Cmakelist and code

--- src/motion_planning.cpp
+++ src/motion_planning.cpp
@@ -8,13 +8,16 @@
 
 //THIS COMMENT IS A TEST
 
+
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "move_turtle");
 
-    int width_room = 500;
-    int length_room = 700;
-    int nexhib = 0;
+    //int width_room = 500;
+    //int length_room = 700; Currently not necessary - Used to be dimensions for the room
+
+    int nexhib = 0, robx = 0, roby = 0, x = 0, y = 0;
+    int robotpos[2] ={};
 
     std::cout << "Welcome user. How many exhibition points exist in the room?" << std::endl;
     std::cin >> nexhib;
@@ -26,13 +29,13 @@ int main(int argc, char *argv[])
 
         //for(int k = 0; k<2; k++){  //This forloop loops through columns
 
-        int x = 0, y = 0, exhibwidth = 0, exhiblength = 0;
+        
 
         std::cout << "what is the x coordinate? (cm) for the " << (i + 1);
         std::cout << "th point?" << std::endl;
         std::cin >> x;
 
-        while (0 > x || x > length_room)
+        while (0 > x)
         {
             std::cout << "exhibiton is too big for room dum dum. Please enter a new value for x" << std::endl;
             std::cin >> x;
@@ -42,7 +45,7 @@ int main(int argc, char *argv[])
         std::cout << "th point?" << std::endl;
         std::cin >> y;
 
-        while (0 > y || y > width_room)
+        while (0 > y)
         {
             std::cout << "The value for y exceeds the width of the room, dum dum. Please enter a new value for y" << std::endl;
             std::cin >> y;
@@ -54,7 +57,29 @@ int main(int argc, char *argv[])
 
         //}
     }
+        std::cout << "\n";
+        std::cout << "Great. Based on your exhibit coordinates you'll now have to enter the coordinates for where the robot will be positioned, according to the other exhibits (As unsigned integer):" << std::endl;
+        std::cout << "\n";
+        std::cout << "Enter x-coordinate for robot position" << std::endl;
+        std::cin >> robx;
 
+        while (0 > robx)
+        {
+            std::cout << "The entered x-coordinate for the robot was negative. Try again" << std::endl;
+            std::cin >> robx;
+        }
+
+        std::cout << "Enter Y-coordinate for robot position" << std::endl;
+        std::cin >> roby;
+
+        while (0 > roby)
+        {
+            std::cout << "The entered y-coordinate for the robot was negative. Try again" << std::endl;
+            std::cin >> roby;
+        }
+
+
+//The following code isn't a part of the pseudo-code. With this we just check the stored user input
     for (int p = 0; p < nexhib; p++) // Iterate over rows
     {
         for (int o = 0; o < 2; o++) // Iterate over elements within the row
@@ -64,67 +89,6 @@ int main(int argc, char *argv[])
         std::cout << std::endl; // A new line for a new row
     }
 
-
-
-
-
-    
-
-
-
-
-
-
-    ros::NodeHandle nh;
-
-    ros::service::waitForService("/turtle1/teleport_absolute", -1);
-
-    ros::ServiceClient teleport_client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
-    ros::ServiceClient pen_client = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
-
-    ros::Duration(2.0).sleep();
-
-    turtlesim::SetPen pen_srv;
-    pen_srv.request.off = true;
-    pen_client.call(pen_srv);
-
-    turtlesim::TeleportAbsolute srv;
-
-    srv.request.x = 0.0;
-    srv.request.y = 0.0;
-    teleport_client.call(srv);
-    pen_srv.request.off = false;
-    pen_srv.request.width = 10;
-    pen_client.call(pen_srv);
-
-    srv.request.x = 0;
-    srv.request.y = width_room; //neeeeeeejjjjjjjjjjjj
-    teleport_client.call(srv);
-
-    srv.request.x = length_room;
-    srv.request.y = width_room;
-    teleport_client.call(srv);
-
-    srv.request.x = length_room;
-    srv.request.y = 0;
-    teleport_client.call(srv);
-
-    srv.request.x = 0;
-    srv.request.y = 0;
-    teleport_client.call(srv);
-
-    pen_srv.request.off = true;
-    pen_client.call(pen_srv);
-
-    for (int u = 0; u < nexhib; u++) // Iterate over rows
-    {
-        pen_srv.request.off = true;
-        srv.request.x = array[u][0];
-        srv.request.y = array[u][1];
-        teleport_client.call(srv);
-
-        pen_srv.request.off = false;
-    }
 
     return 0;
 }
