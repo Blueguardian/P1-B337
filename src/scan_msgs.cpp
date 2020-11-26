80fa308c250e3a760f6866bb9b5f59d05f510996
dadihrannar1 <ddavid20@student.aau.dk>
2020-11-26 09:26:22 +0100

    new branch

--- src/scan_msgs.cpp
+++ src/scan_msgs.cpp
@@ -1,16 +1,21 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+/*Here, we include the sensor_msgs/LaserScan message that we want to send over the wire.*/
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "laser_scan_publisher");
 
+
   ros::NodeHandle n;
   ros::Publisher scan_pub = n.advertise<sensor_msgs::LaserScan>("scan", 50);
+/*This code creates a ros::Publisher that is later used to send LaserScan messages using ROS.*/
+
 
   unsigned int num_readings = 100;
   double laser_frequency = 40;
   double ranges[num_readings];
   double intensities[num_readings];
+  /*Here we're just setting up storage for the dummy data that we're going to use to populate our scan. A real application would pull this data from their laser driver.*/
 
   int count = 0;
   ros::Rate r(1.0);
@@ -21,6 +26,7 @@ int main(int argc, char** argv){
       intensities[i] = 100 + count;
     }
     ros::Time scan_time = ros::Time::now();
+  /*Populate the dummy laser data with values that increase by one every second.*/
 
     //populate the LaserScan message
     sensor_msgs::LaserScan scan;
@@ -39,8 +45,10 @@ int main(int argc, char** argv){
       scan.ranges[i] = ranges[i];
       scan.intensities[i] = intensities[i];
     }
+/*Create a scan_msgs::LaserScan message and fill it with the data that we've generated in preparation to send it over the wire.*/
 
     scan_pub.publish(scan);
+    /*Publish the message over ROS.*/
     ++count;
     r.sleep();
   }
